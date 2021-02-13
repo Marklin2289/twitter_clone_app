baa859ffd69590bdf209475b0ba072f3f471769b
Mark Lin <marklin2289@gmail.com>
2021-02-12 19:26:39 -0800

    added firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHa6dJgKH73KknU6LL4SPXkoo-rPGvNDA",
+  authDomain: "twitter-clone-app-ae21c.firebaseapp.com",
+  projectId: "twitter-clone-app-ae21c",
+  storageBucket: "twitter-clone-app-ae21c.appspot.com",
+  messagingSenderId: "817744390321",
+  appId: "1:817744390321:web:e162c98b7c5491429e1a90",
+  measurementId: "G-Q79T57X2R9",
+};
+export default firebase.initializeApp(firebaseConfig);
