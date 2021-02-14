fc9e27686c3785522d1b7eee998bde433b99a33c
Mark Lin <marklin2289@gmail.com>
2021-02-14 13:25:49 -0800

    added tweet feature and firestore

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,30 @@
-import Navigation from 'components/Navigation';
-import React from 'react';
+import { dbService } from 'fbase';
+import React, { useState } from 'react';
 
-export default function home() {
+export default function Home() {
+    const [tweet,setTweet] = useState("");
+
+    const onSubmit = async(e) => {
+        e.preventDefault();
+        await dbService.collection("tweet").add({
+            tweet: tweet,
+            createdAt: Date.now()
+        });
+        setTweet("");
+    }
+
+    const onChange = (e) => {
+        const {target: {value}} = e;
+        setTweet(value);
+        // console.log(value)
+    }
+
     return (
-        
-        <span>home page</span>
+        <div>
+            <form onSubmit={onSubmit} >
+                <input  value={tweet} onChange={onChange} type="text" placeholder="what's in your mind?" maxLength={120} />
+                <input type="submit" value="tweet" />
+            </form>
+        </div>
     )
 };
